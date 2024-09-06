6d951db1cf0f0a21cb14eb288b1fffad4bfaa56a
Julien de Castelnau <julien.decastelnau@epfl.ch>
2024-09-06 16:27:46 +0200

    optimizations to eqn

--- ckt-convert/src/eqn.rs
+++ ckt-convert/src/eqn.rs
@@ -3,11 +3,12 @@ use std::{collections::HashMap, path::PathBuf};
 use crate::parse::Token;
 use crate::parse;
 
-fn postfix_to_expanded(postfix: Vec<Token>, expr_dict: &HashMap<String, String>) -> String {
+fn expand_postfix(postfix: &mut Vec<Token>, expr_dict: &HashMap<String, Vec<Token>>) -> String {
     let mut output_str = String::new();
     let mut op_cnt_stack: Vec<i32> = Vec::new();
     let mut op_cnt = -1;
-    for token in postfix.iter().rev() {
+    while !postfix.is_empty() {
+        let token = postfix.pop().unwrap();
         match token {
             Token::And => {
                 op_cnt_stack.push(op_cnt);
@@ -30,10 +31,17 @@ fn postfix_to_expanded(postfix: Vec<Token>, expr_dict: &HashMap<String, String>)
                 output_str.push_str("(!")
             }
             Token::Ident(ident) => {
-                op_cnt -= 1;
-                match expr_dict.get(ident) {
-                    Some(e) => output_str.push_str(e),
-                    None => output_str.push_str(ident),
+                match expr_dict.get(&ident) {
+                    Some(tokens) => {
+                        for token in tokens {
+                            postfix.push(token.clone());
+                        }
+                        continue;
+                    }
+                    None => { 
+                        output_str.push_str(&ident);
+                        op_cnt -= 1;
+                    }
                 }
                 
             }
@@ -55,7 +63,7 @@ pub fn convert_eqn(ineqn: PathBuf, outsexpr: PathBuf, outnode: &str) {
     let lines = std::fs::read_to_string(ineqn).unwrap();
 
     let mut state: bool = false;
-    let mut expr_dict: HashMap<String, String> = HashMap::new();
+    let mut expr_dict: HashMap<String, Vec<Token>> = HashMap::new();
     for line in lines.lines() {
         if !state {
             state = (!line.contains("INORDER") && !line.contains("OUTORDER")) && line.contains("=");
@@ -63,16 +71,54 @@ pub fn convert_eqn(ineqn: PathBuf, outsexpr: PathBuf, outnode: &str) {
         if state && line.contains("=") {
             let mut split = line.split("=");
             let lhs = String::from(split.next().unwrap().trim());
-            let rhs = postfix_to_expanded(
-                parse::infix_str_to_postfix(split.next().unwrap()),
-                &expr_dict,
-            );
-            expr_dict.insert(lhs.clone(), rhs);
+            let postfix = parse::infix_str_to_postfix(split.next().unwrap());
+            expr_dict.insert(lhs, postfix);
         }
     }
     // take the last output for the time being
-    let contents = expr_dict.get(outnode).unwrap_or_else(|| {panic!("Could not find node {} in circuit!", outnode);});
+    let mut top_postfix = expr_dict.remove(outnode).unwrap_or_else(|| {panic!("Could not find node {} in circuit!", outnode);});
+    let contents = expand_postfix(
+        &mut top_postfix,
+        &expr_dict,
+    );
     std::fs::write(outsexpr, contents).unwrap();
 }
 
 
+/*
+mod tests {
+    use crate::parse::infix_str_to_postfix;
+    use super::parse::Token::*;
+    use super::postfix_to_expanded;
+
+    #[test]
+    fn test_01() {
+        let inp_string = "(n1010 * !n1009) + (!n1010 * n1009);";
+        let postfix = infix_str_to_postfix(&inp_string);
+        let expanded = postfix_to_expanded(postfix, expr_dict)
+        assert!(postfix == vec![
+            Ident(
+                String::from("i25"),
+            ),
+            Not,
+            Ident(
+                String::from("i24"),
+            ),
+            Not,
+            And,
+            Ident(
+                String::from("i26"),
+            ),
+            Not,
+            And,
+            Ident(
+                String::from("i27"),
+            ),
+            Not,
+            And,
+        ])
+    }
+}
+
+
+*/
